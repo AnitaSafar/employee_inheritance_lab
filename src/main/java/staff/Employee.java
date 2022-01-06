5d384544c75a093b4bfb6637bcad7d9eef95b741
Anita Safar <ancsa0416@gmail.com>
2022-01-06 14:50:16 +0000

    employee methods done, management package and manager class created

--- src/main/java/staff/Employee.java
+++ src/main/java/staff/Employee.java
@@ -4,12 +4,38 @@ public abstract class Employee {
 
     private String name;
     private String NI;
-    private int salary;
+    private double salary;
 
-    public Employee(String name, String NI, int salary) {
+    public Employee(String name, String NI, double salary) {
         this.name = name;
         this.NI = NI;
         this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNI() {
+        return NI;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double newSalary){
+        this.salary = newSalary;
+    }
+
+    public void raiseSalary(double raise){
+//        double salary = this.getSalary();
+        double newSalary = this.getSalary() + raise;
+        this.setSalary(newSalary);
+    }
+
+    public double payBonus(){
+        return this.salary*0.01;
     }
 
 
---
+++ src/test/java/EmployeeTest.java
@@ -0,0 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.Employee;
+
+import static org.junit.Assert.assertEquals;
+
+public class EmployeeTest {
+
+    Employee employee;
+
+//    @Before
+//    public void before() {
+//        employee = new Employee("Jack Smith", "893485834TS", "36000.25");
+//    }
+//
+//    @Test
+//    public void canRaiseSalary() {
+//        assertEquals("36000.25", employee.raiseSalary());
+//    }
+
+
+}
